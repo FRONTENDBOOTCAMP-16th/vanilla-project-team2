242df47951bc5736abc5b23d6be8d0e9682db417
smj <smj123432@gmail.com>
2026-02-25 19:21:26 +0900

    refactor: qna, 자습방 bem 방식에 맞춰 클래스명 수정 그에 따른 css js도 맞춰서 수정

--- src/js/components/postItem.js
+++ src/js/components/postItem.js
@@ -2,27 +2,27 @@ import { timeForToday } from '../utils/date.js'
 
 export const postItem = (post) => {
   return `
-    <li class="main-post__item" data-category="${post.typeIndex}" data-id="${post.post_id}">
-  <a href="#" class="main-post__inner">
-    
-    <div class="main-post__top-row">
-      <span class="main-post__tag">${post.type}</span>
-      <span class="main-post__date">${timeForToday(post.create_date)}</span>
-    </div>
+    <li class="post__item" data-category="${post.typeIndex}" data-id="${post.post_id}">
+      <a href="#" class="post__inner">
 
-    <div class="main-post__group">
-      <h3 class="main-post__heading">${post.subject}</h3>
-      <p class="main-post__text">${post.contents}</p>
-    </div>
-    
-    <div class="main-post__meta-box">
-      <div class="main-post__author-row">
-        <span class="main-post__comment-count">${post.commentCount}개의 댓글</span>
-        <span class="main-post__author-text">by <strong>${post.user_nickname}</strong></span>
-      </div>
-    </div>
-    
-  </a>
-</li>
+        <div class="post__top-row">
+          <span class="post__tag">${post.type}</span>
+          <span class="post__date">${timeForToday(post.create_date)}</span>
+        </div>
+
+        <div class="post__group">
+          <h3 class="post__heading">${post.subject}</h3>
+          <p class="post__text">${post.contents}</p>
+        </div>
+
+        <div class="post__meta-box">
+          <div class="post__author-row">
+            <span class="post__comment-count">${post.commentCount}개의 댓글</span>
+            <span class="post__author-text">by <strong>${post.user_nickname}</strong></span>
+          </div>
+        </div>
+
+      </a>
+    </li>
   `
 }
